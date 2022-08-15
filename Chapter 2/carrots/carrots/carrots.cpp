05237e2819f255253c59a333cb30ef3f1125e89b
zhenli1347 <2503248255@qq.com>
2022-08-15 21:47:01 +0800

    .
    .

---
+++ Chapter 2/carrots/carrots/carrots.cpp
@@ -0,0 +1,35 @@
+﻿
+// carrots.cpp -- food processing program
+// uses and displays a variable
+
+#include <iostream>
+//using namespace std;      
+//namespace-命名空间 解决名称冲突；只能在全局范围内进行定义，可嵌套，
+//  可实现命名空间中函数的定义与实现相分离（实现在命名空间之外）；
+int main()
+{
+    using namespace std;
+
+    int carrots;            // declare an integer variable
+
+    carrots = 25;            // assign a value to the variable
+    cout << "I have ";
+    cout << carrots;        // display the value of the variable
+    cout << " carrots.";
+    cout << endl;
+    carrots = carrots - 1;  // modify the variable
+    cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+    // cin.get();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
